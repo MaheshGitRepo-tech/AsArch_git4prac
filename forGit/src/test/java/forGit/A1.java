3ee1154c7d83f3ecec29c832205ffeeb2860f99c
Lenovo <Lenovo@Mahesh>
2025-02-10 12:41:47 +0530

    commit it now

--- forGit/src/test/java/forGit/A1.java
+++ forGit/src/test/java/forGit/A1.java
@@ -1,9 +1,17 @@
 package forGit;
 
 public class A1 {
-public static vooid main(String[] args){
+public static void main(String[] args){
 	 
 	
 	System.out.println("i am hari as arch sending this....");
+	
+	System.out.println("i am starting new ");
+	
+	System.out.println("i have updated it and completed ");
+	
+	System.out.println("if ur seeeing this message that means ur are done every thing right");
+	
+	
 }
 }
